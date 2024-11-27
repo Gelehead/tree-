1be02d99ad65c7b9f26ec10dc10ef85489ffa43a
Gelehead <113441042+Gelehead@users.noreply.github.com>
2024-11-27 15:34:43 -0500

    somewhat better generateSegmentHermite

--- TP3_Geometry.js
+++ TP3_Geometry.js
@@ -9,7 +9,7 @@ class Node {
 		this.a0 = null; //Rayon de la branche a p0
 		this.a1 = null; //Rayon de la branche a p1
 
-		this.sections = null; //Liste contenant une liste de points representant les segments circulaires du cylindre generalise
+		this.sections = []; //Liste contenant une liste de points representant les segments circulaires du cylindre generalise
 	}
 }
 
@@ -47,84 +47,94 @@ TP3.Geometry = {
 			}
 		}
 	},
+
+	gothroughtree: function (rootNode){
+		console.log(rootNode);
+		for (let i = 0; i < rootNode.childNode.length; i++) {
+			this.gothroughtree(rootNode.childNode[i]);
+		}
+	},
 	
 
 	generateSegmentsHermite: function (rootNode, lengthDivisions = 4, radialDivisions = 8) {
-		if ( rootNode.parentNode == null ) {
-			this.generateSegmentsHermite(rootNode.childNode[0]);
-			return rootNode;
+		if (rootNode.parentNode == null) {
+			for (let i = 0; i < rootNode.childNode.length; i++) {
+				this.generateSegmentsHermite(rootNode.childNode[i]);
+			}
 		} else {
 			for (let i = 0; i < rootNode.childNode.length; i++) {
 				this.generateSegmentsHermite(rootNode.childNode[i]);
 			}
-			p = rootNode.parentNode;
-			gp = p.parentNode;
+	
+			let p = rootNode.parentNode;
+			let h0 = rootNode.p1.clone();
+			let h1 = p.p0.clone();
 
-			h0 = rootNode.p0;
-			h1 = gp.p0;
+			console.log("h1", h1);
+	
+			// potentiel probleme
+			let v0 = rootNode.p0.clone().sub(rootNode.p1).normalize();
+			let v1 = p.p0.clone().sub(p.p1).normalize();
+	
+			for (let t = 0; t <= 1; t += 1 / lengthDivisions) {
+				let { p: pt, dp } = this.hermite(h0, h1, v0, v1, t);
 
-			let v0 = new THREE.Vector3(
-				rootNode.p1.x - rootNode.p0.x,
-				rootNode.p1.y - rootNode.p0.y,
-				rootNode.p1.z - rootNode.p0.z
-			);
-			
-			let v1 = new THREE.Vector3(
-				gp.p1.x - gp.p0.x,
-				gp.p1.y - gp.p0.y,
-				gp.p1.z - gp.p0.z
-			);
-
-			for (let t = 0; t < 1; t += 1/lengthDivisions) {
-				// point coordinates (p) and speed (dp) at t [0,1] on the curve
-				p, dp = this.hermite(h0, h1, v0, v1, t);
-				dp = normalize(dp);
-				// arbitrary vector to get normal vectors
-				let r = new Vector3(0,0,1);
-				var n1 = normalize(cross(r,dp));
-				var n2 = normalize(cross(dp, n1));
-
-				//get the points around the radialDivision sided polygon
-				var lengthI = 0;
-				var theta = 0;
-				for (let i = 0; i < radialDivisions ; i++){
-					theta = (2*Math.PI*i) / radialDivisions;
-					// linear interpolation of radius
-					lengthI = (i)*rootNode.a1 + (1-i)*gp.a0;
-					rootNode.sections.append(p + lengthI*(Math.cos(theta)*n1 + Math.sin(theta) * n2));
+				console.log(t, pt);
+	
+				dp.normalize();
+				let r = new THREE.Vector3(0, 0, 1);
+				if (Math.abs(dp.dot(r)) > 0.99) {
+					r.set(1, 0, 0); // Avoid collinearity
 				}
+	
+				let n1 = new THREE.Vector3().crossVectors(r, dp).normalize();
+				let n2 = new THREE.Vector3().crossVectors(dp, n1).normalize();
+	
+				let pointList = [];
+				for (let i = 0; i < radialDivisions; i++) {
+					let theta = (2 * Math.PI * i) / radialDivisions;
+					let lengthI = ((radialDivisions - i) / radialDivisions) * rootNode.a1 + 
+								  (i / radialDivisions) * p.a0;
+	
+					let offset = n1.clone().multiplyScalar(Math.cos(theta) * lengthI)
+						.add(n2.clone().multiplyScalar(Math.sin(theta) * lengthI));
+					pointList.push(pt.clone().add(offset));
+				}
+				if (!rootNode.sections) rootNode.sections = [];
+				rootNode.sections.push(pointList);
 			}
 		}
 	},
+	
 
 	hermite: function (h0, h1, v0, v1, t) {
-		// calculating pt
-		var p0 = 2 * Math.pow(t, 3) - 3 * Math.pow(t, 2) + 1;
-		var p1 = -2 * Math.pow(t, 3) - 3 * Math.pow(t, 2);
-		var r0 = Math.pow(t, 3) - 2*Math.pow(t, 2) + t;
-		var r1 = Math.pow(t, 3) - Math.pow(t, 2);
+	
+		// Calculate pt
+		let p0 = 2 * Math.pow(t, 3) - 3 * Math.pow(t, 2) + 1;
+		let p1 = -2 * Math.pow(t, 3) + 3 * Math.pow(t, 2);
+		let r0 = Math.pow(t, 3) - 2 * Math.pow(t, 2) + t;
+		let r1 = Math.pow(t, 3) - Math.pow(t, 2);
+	
+		let pt = h0.clone().multiplyScalar(p0)
+			.add(h1.clone().multiplyScalar(p1))
+			.add(v0.clone().multiplyScalar(r0))
+			.add(v1.clone().multiplyScalar(r1));
+	
+		// Calculate dpt
+		let dp0 = (6 * Math.pow(t, 2)) - (6 * t);
+		let dp1 = (-6 * Math.pow(t, 2)) + (6 * t);
+		let dr0 = (3 * Math.pow(t, 2)) - (4 * t) + 1;
+		let dr1 = (3 * Math.pow(t, 2)) - (2 * t);
+	
+		let dpt = h0.clone().multiplyScalar(dp0)
+			.add(h1.clone().multiplyScalar(dp1))
+			.add(v0.clone().multiplyScalar(dr0))
+			.add(v1.clone().multiplyScalar(dr1))
+		;
 
-		pt = h0 * p0
-		   + h1 * p1
-		   + v0 * r0
-		   + v1 * r1
-	 	;
-
-		// calculating dt
-		var dp0 = 6 * Math.pow(t, 2) - 6 * t;
-		var dp1 = -6 * Math.pow(t, 2) + 6 * t;
-		var dr0 = 3 * Math.pow(t, 2) - 4 * t + 1;
-		var dr1 = 3 * Math.pow(t, 2) - 2 * t;
-
-		dpt = h0 * dp0
-		   + h1 * dp1
-		   + v0 * dr0
-		   + v1 * dr1
-	 	;
-
-		return [pt, dpt]
-
+		return { p: pt, dp: dpt }; // Return as an object for clarity
 	},
+	
 
 
 	// Trouver l'axe et l'angle de rotation entre deux vecteurs
